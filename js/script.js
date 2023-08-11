ac91097a0b170a0994544d2a26fd89f8ee4917ea
DinaSharipova <sharipova0308@gmail.com>
2023-08-11 11:37:47 +0500

    add mobile

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu'),
     menuItem = document.querySelectorAll('.menu__item'),
-    hamburger = document.querySelector('.hamburger');
+    hamburger = document.querySelector('.hamburger'),
+    body = document.querySelector('body');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
         menu.classList.toggle('menu_active');
+        body.classList.toggle('overflow');
     });
 
     menuItem.forEach(item => {
